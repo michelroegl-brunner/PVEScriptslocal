178eee664a75d6aabdf95cb257fca954c5e181bd
Rögl-Brunner Michel <michel.roegl-brunner@htl-braunau.at>
2025-09-09 14:04:07 +0200

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default async function Home() {
+
+  return (
+    
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gray-400 text-white">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">PVE Scripts local</h1>
+        </div>
+      </main>
+
+  );
+}
